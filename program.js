7826c24cfbdfa3f9447f60610318268b5f22866f
Jacek <jacek.klosinski@gmail.com>
2017-08-05 00:20:34 +0200

    bootcamp_js_modul.1.3

---
+++ program.js
@@ -0,0 +1,31 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        //var instruction = input.toString().trim();
+        switch (input.toString().trim()) {
+            case '/exit':
+                {
+                    process.stderr.write('Quitting app!\n');
+                    process.exit();
+                }
+            case '/node version':
+                {
+                    var nodeVer = process.versions.node;
+                    process.stderr.write(nodeVer);
+                    break;
+                }
+            case '/system language':
+                {
+                    //w materiałach ze szkolenia jest informacja, że w process.env powinno być zawarte LANG ale je tego nie widzę...
+                    var systemLang = JSON.stringify(process.env);
+                    process.stderr.write(systemLang);
+                    break;
+                }
+            default:
+                process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
