b9575b3bae8d46bfcb32d70de14602be5ee8bc84
JacekBartlomiej <jacek.klosinski@gmail.com>
2017-08-08 23:24:33 +0200

    Zadanie 1.8

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.write(data);
+            response.end();
+        });
+    } else {
+        fs.readFile('./404.jpg', function(err, data) {
+            response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            response.end(data);
+        });
+    }
+});
+
+server.listen(8080);
