4e1d39685ab1570326be98fac6c93203db1320bf
Michał Gacek <gacek.michael@outlook.com>
2017-08-09 21:03:59 +0200

    Improvements

--- modules/TimeFormat.js
+++ modules/TimeFormat.js
@@ -1,29 +1,10 @@
-function formatTime(seconds) {
-    var secPerMinute = 60;
-    var secPerHour = 60 * 60;
+function formatTime(total) {
+    var minutes = total / 60,
+        seconds = Math.round(total % 60),
+        hours = Math.round(minutes / 60),
+        minutes = Math.round(minutes % 60);
 
-    if (seconds > secPerHour) {
-        return showHourMinSec(seconds);
-    } else if (seconds > secPerMinute) {
-        return showMinSec(seconds);
-    } else {
-        return seconds + " sekund";
-    }
-
-    //Widzę, że te dwie metody mają powtarzalny kod. Podpowiesz proszę, jak sprawić, żeby nie było powtórzenia?
-    function showMinSec(seconds) {
-        var minutes = Math.floor(seconds / secPerMinute);
-        var remainingSeconds = seconds - (minutes * secPerMinute);
-        var result = minutes + " minutes and " + remainingSeconds + " seconds";
-        return result;
-    }
-
-    function showHourMinSec(seconds) {
-        var hours = Math.floor(seconds / secPerHour);
-        var remainingMinSec = seconds - (hours * secPerHour);
-        var result = hours + " hours and " + showMinSec(remainingMinSec);
-        return result;
-    }
+    return hours + " hours " + minutes + " minutes " + seconds + " seconds";
 }
 
 exports.calculate = formatTime;
